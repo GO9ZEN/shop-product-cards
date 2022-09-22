3976dff11e7cd10e2be155041d8b7b1a909f1d4a
GO9ZEN <sarasaanjana2002@gmail.com>
2022-09-22 09:18:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import ShopProductCard from './ShopProductCard/ShopProductCard';
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className="image">
+        <ShopProductCard />
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
+
+// shop-product-cards
---
+++ src/ShopProductCard/ShopProductCard.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ShopCard from "./ShopCard/ShopCard";
+import ProductCard from "./ProductCard/ProductCard";
+
+import "./ShopProductCard.css";
+
+function ShopProductCard() {
+  return (
+    <div>
+        <div className="header">
+            <div>
+                <span>Shop-Product-Card</span>
+            </div>
+        </div>
+
+        <div className="shop-card">
+            <div className="shop-card-topic-part">
+                <h4 className="shop-card-topic-part-h4">Shop Card</h4>
+            </div>
+
+            <div className="shop-card-hr-part"></div>
+
+            <div className="shop-card-product-part">
+                <ShopCard />
+            </div>
+        </div>
+
+        <div className="product-card">
+            <div className="product-card-topic-part">
+                <h4 className="product-card-topic-part-h4">Product Card</h4>
+            </div>
+
+            <div className="product-card-hr-part"></div>
+
+            <div className="product-card-product-part">
+                <ProductCard />
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default ShopProductCard;
